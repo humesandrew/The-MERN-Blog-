be97c35bde8fb78bb59204776ce80833e5065ca8
Andy Humes <andrew.humes@du.edu>
2022-10-28 12:09:37 -0600

    addhomepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//import pages and components//
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+          <Route 
+            path="/"
+            element={<Home />}>
+          </Route>
+        </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }

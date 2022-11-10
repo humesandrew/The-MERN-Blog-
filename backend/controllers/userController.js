851f9538f1a0688103a09162ed12dc9b307e7014
Andy Humes <andrew.humes@du.edu>
2022-11-09 17:24:46 -0700

    addjwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,14 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+
+//create a createToken so we can use it with our requests // 
+// we'll create (sign) a token with a payload (date we take in, below is _id only)//
+// , a secret (used for signature), and any options (like expires in 3 days)//
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '2d'})
+}
+
 
 
 //login user //       
@@ -15,7 +25,13 @@ const {email, password} = req.body
 
 try {
     const user = await User.signup(email, password)
-    res.status(200).json({email, user})
+   
+   // after we've saved them to the db, we create a token for them//
+const token = createToken(user._id)
+   
+    res.status(200).json({email, token})
+
+
 
 } catch (error) {
     res.status(400).json({error: error.message})
